279d75682f5d1af1aaae446ef4b79c09d95b04ed
Dathuong812 <tranthidathuong812@gmail.com>
2023-10-23 20:36:03 +0700

    update

---
+++ check.js
@@ -0,0 +1,25 @@
+function check(){
+    var v = document.getElementById("tinh").value;
+    switch (v) {
+   case "1":
+       window.location.href = 'miennui.html';
+       break;
+   case "2":
+       window.location.href = 'dbbb.html';
+       break;
+   case "3":
+   window.location.href = 'btb.html';
+       break;
+   case "4":
+   window.location.href = 'dhntb.html';
+       break;
+   case "5":
+   window.location.href = 'tayngyen.html';
+       break;
+   case "6":
+   window.location.href = 'dnb.html';
+       break;
+   case "7":
+   window.location.href = 'dbscl.html';
+   }
+}
